c49514895f7252c01157a7fe5bc741b337c5edd6
lauratobon <52606975+lauratobon@users.noreply.github.com>
2025-06-24 22:30:39 -0500

    Update AddContactTest.scala

--- src/test/scala/Demo/AddContactTest.scala
+++ src/test/scala/Demo/AddContactTest.scala
@@ -3,24 +3,23 @@ package Demo
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import Demo.Data._
-import Demo.LoginActions._ 
+import Demo.LoginActions._ // Importa el objeto con las acciones de login
 
 class AddContactTest extends Simulation{
 
   // 1 Http Conf
   val httpConf = http.baseUrl(url)
     .acceptHeader("application/json")
-    //Verificar de forma general para todas las solicitudes
-    .check(status.is(200))
+    .check(status.is(200)) // Chequeo general para todas las solicitudes en esta simulación
 
-  // 2 Scenario Definition 
-  val scn = scenario("Create contact").
-    .exec(userLogin) // Primero, realizo el login para obtener el authToken y guardarlo en la sesión
-    .exec(http("Create Contact Request") // Luego, hago la petición para crear el contacto
-      .post("contacts")
-      .header("Authorization", "Bearer ${authToken}") // Usa el authToken guardado en la sesión
-      .body(StringBody(s"""{ "firstName": "Lau", "lastName": "Tobon", "birthdate": "1970-01-01", "email": "jdoe@fake.com", "phone": "8005555555", "street1": "1 Main St.", "street2": "Apartment A", "city": "Anytown", "stateProvince": "KS", "postalCode": "12345", "country": "USA" }""")).asJson
-      .check(status.is(201)) // Espera un 201 Created para esta petición específica
+  // 2 Scenario Definition
+  val scn = scenario("Create contact").exec(userLogin) // La primera acción .exec() va aquí
+    .exec( 
+      http("Create Contact Request")
+        .post("contacts")
+        .header("Authorization", "Bearer ${authToken}") // Usa el authToken guardado en la sesión
+        .body(StringBody(s"""{ "firstName": "Lau", "lastName": "Tobon", "birthdate": "1970-01-01", "email": "jdoe@fake.com", "phone": "8005555555", "street1": "1 Main St.", "street2": "Apartment A", "city": "Anytown", "stateProvince": "KS", "postalCode": "12345", "country": "USA" }""")).asJson
+        .check(status.is(201)) // Espera un 201 Created para esta petición específica
     )
 
   // 3 Load Scenario
